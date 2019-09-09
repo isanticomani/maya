2d62be34931a78cf70159effc17b9ea803349b9c
Ivan Santicomani <iss.30692@gmail.com>
2019-09-09 13:00:51 -0500

    Aun no termino el dibujo

--- dibuja.php
+++ dibuja.php
@@ -8,13 +8,37 @@ if($number < 0){
     die;
 }else{
     $draw = "";
-    
-    ?>
-    <pre>
+    $h = 9;
+    $v = 5;
+    for ($j=0; $j <= $v; $j++) { 
+        for ($i=0; $i <= $h * $number; $i++) { 
+            var_dump("j -> $j");
+            var_dump("i -> $i");
+            if ($j == 0 && $i == 0) {
+                $draw .= " ";
+            }else if ($j == 0 && $i == $h * $number) {
+                $draw .= "\n";
+            }else if ($j == 0 && $i > 0) {
+                $draw .= "_";
+            }else if ($j == 0 && $i == $h * $number) {
+                $draw .= "\n";
+            }else if ($j > 0 && $i == 0) {
+                $draw .= "|";
+            }else if ($j > 0 && $j < $v && $i < $h * $number) {
+                $draw .= " ";
+            }else if ($j > 0 && $i == $h * $number) {
+                $draw .= "|\n";
+            }else if ($j == $v && $i > 0) {
+                $draw .= "_";
+            }
+            ?>
+<pre>
 <?php
 echo $draw;
 ?>
-    </pre>
-    <?php
+</pre>
+            <?php
+        }
+    }
 }
 ?>
